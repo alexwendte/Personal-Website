e689ecf1dfe7a113f14fdd03736a3a78725666a1
Phillip (Alex) Wendte <themostcolm@gmail.com>
2017-11-26 02:32:13 -0600

    Added some content to all pages, especially home. Fixed the sidenav a bit. Did a build So I could see how to host my website online

---
+++ public/assets/scripts.js
@@ -0,0 +1,15 @@
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "20rem";
+  document.getElementById("main").style.marginRight = "20rem";
+  document.getElementById("navheader").style.display = "none";
+  document.getElementById("navhome").style.display = "inline-block";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginRight = "0";
+  document.getElementById("navheader").style.display = "inline-block";
+  document.getElementById("navhome").style.display = "none";
+}
